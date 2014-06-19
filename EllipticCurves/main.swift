b3c2b716bb824965a73571757ca47329802c267c
Berkus <berkus@gmail.com>
2014-06-19 08:22:05 +0300

    Initial Commit

---
+++ EllipticCurves/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EllipticCurves
+//
+//  Created by Berkus on 19/06/14.
+//  Copyright (c) 2014 Berkus. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
